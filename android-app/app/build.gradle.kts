be09f6e73152d156a199b5edfd1b24f9ad75a846
Safwen Baroudi <safwen.baroudi@contentsquare.com>
2026-04-06 07:12:11 +0200

    v2.38: Reduce header vertical space

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.tunisianprayertimes"
         minSdk = 26
         targetSdk = 36
-        versionCode = 48
-        versionName = "2.37"
+        versionCode = 49
+        versionName = "2.38"
         
         androidResources {
             localeFilters += "ar"
